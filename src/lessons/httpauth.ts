3d537102ab10a340396788921a5b3de4ed09fc88
Basarat <basaratali@gmail.com>
2020-05-12 11:20:39 +1000

    wrap

--- src/lessons/httpauth.ts
+++ src/lessons/httpauth.ts
@@ -4,19 +4,23 @@ async function main() {
   const browserType = playwright.chromium;
 
   // Launch browser
-  const browser = await browserType.launch({
-    headless: false
-  });
+  const browser = await browserType.launch();
   // Create a context
   const context = await browser.newContext();
   // Create a page
   const page = await context.newPage();
 
-  // Carry out actions
+  // Provide http credentials
   context.setHTTPCredentials({
     username: 'username',
     password: 'password'
   });
-  page.goto('http://localhost:3000/auth');
+
+  // Carry out actions
+  await page.goto('http://localhost:3000/auth');
+  await page.screenshot({ path: 'output/screenshot.png' });
+
+  // Close the browser
+  await browser.close();
 }
 main();
