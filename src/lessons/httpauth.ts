0fdde1f6574e128c2d4a791e1e4a098ab71eb5f0
Basarat <basaratali@gmail.com>
2020-05-12 10:48:07 +1000

    wrap up

---
+++ src/lessons/httpauth.ts
@@ -0,0 +1,26 @@
+import * as playwright from 'playwright';
+
+async function main() {
+  const browserType = playwright.chromium;
+
+  // Launch browser
+  const browser = await browserType.launch({
+    headless: false
+  });
+  // Create a context
+  const context = await browser.newContext();
+  // Create a page
+  const page = await context.newPage();
+
+  // Carry out actions
+  context.setHTTPCredentials({
+    username: 'username',
+    password: 'password'
+  });
+  page.goto('http://localhost:3000');
+  page.screenshot({ path: 'output/screenshot.png' });
+
+  // Close the browser
+  await browser.close();
+}
+main();
