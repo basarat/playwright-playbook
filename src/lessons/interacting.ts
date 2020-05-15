ce433b361b99012292ac2e49b58fcce0b0e77c3d
Basarat <basaratali@gmail.com>
2020-05-15 13:10:49 +1000

    wrap

--- src/lessons/interacting.ts
+++ src/lessons/interacting.ts
@@ -10,7 +10,7 @@ async function main() {
   const context = await browser.newContext();
   // Create a page
   const page = await context.newPage();
-  
+
   // Carry out actions
   const url = 'http://localhost:3000/actions';
   await page.goto(url);
@@ -25,6 +25,21 @@ async function main() {
   await page.fill('#key', 'playwright');
   await page.click('#load');
   await page.screenshot({ path: 'output/valid.png' });
+
+  const key = await page.$('#key');
+  const load = await page.$('#load');
+  if (!key) throw new Error('no key input');
+  if (!load) throw new Error('no load button');
+
+  await key.fill('');
+  await load.click();
+  await page.screenshot({ path: 'output/invalid-request-1.png' });
+  await key.fill('invalid key');
+  await load.click();
+  await page.screenshot({ path: 'output/invalid-key-1.png' });
+  await key.fill('playwright');
+  await load.click();
+  await page.screenshot({ path: 'output/valid-1.png' });
 
   // Close the browser
   await browser.close();
